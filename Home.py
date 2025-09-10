ec517ff7a3a508c0760b13d1aa66275c103d94bc
Jayanth Shanmugam <mk.jayanth.s@gmail.com>
2025-09-10 12:43:59 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -20,7 +20,7 @@ with st.sidebar:
                  label = "![Linkedin_Icon](https://img.icons8.com/color/48/linkedin.png) LinkedIn")
     st.page_link("https://github.com/Jayanth-Shanmugam", 
                  label = "![github_Icon](https://img.icons8.com/fluency/96/github.png) Github")
-    st.page_link("Home.py", label = "jmallasa@buffalo.edu", icon=":material/mail:", disabled=True)
+    st.page_link("Home.py", label = "jayanth.mks@gmail.com", icon=":material/mail:", disabled=True)
 
 with st.container():
 
@@ -32,7 +32,7 @@ with st.container():
         st.subheader(":material/waving_hand: Hello!") 
         intro = """
         I'm currently pursuing my **MS in Computer Science and Engineering**, specializing in AI/ML at the :blue[University at Buffalo - SUNY] 
-        (Go Bulls! :the_horns:). My interests lie in **data engineering** and **machine learning**. Previously, I worked as a **Data Engineer** 
+        (Go Bulls!). My interests lie in **data engineering** and **machine learning**. Previously, I worked as a **Data Engineer** 
         at Adani Wilmar Ltd., where I was responsible for setting up efficient ways to move data to our data lake on **Google Cloud** and automate 
         existing **machine learning** workloads. I also interned at the same company during which I led **generative AI** projects, one of which 
         ranked **top 10** among 30,000 teams at the **Google GenAI Hackathon** APAC edition.
